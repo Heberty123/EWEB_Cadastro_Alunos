fb98a501237f11160e5297f8e8c674d02cc7dfb6
Heberty Guelfi do Prado <hebertyprado22005@gmail.com>
2022-10-10 14:33:10 -0300

    interface Aluno was created

---
+++ src/app/aluno.ts
@@ -0,0 +1,6 @@
+export interface Aluno {
+    id: number,
+    name: string,
+    ra: number,
+    cpf: string
+}
--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Aluno } from '../aluno';
 
 @Component({
     selector: 'app-alunos',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./alunos.component.css']
 })
 export class AlunosComponent implements OnInit {
+
+
+    aluno: Aluno = {
+        id: 1,
+        name: 'Heberty Guelfi Do Prado',
+        ra: 1234567891011,
+        cpf: "83756208472"
+    }
+
 
     constructor() {}
 
